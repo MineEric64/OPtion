b96781ecf9d5fac1e8348108fe5b3c71ac1da774
MineEric64 <besteric40@gmail.com>
2021-11-16 22:24:02 +0900

    중간 하다
    포기함

--- OPtion.cs
+++ OPtion.cs
@@ -1,13 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OPtion
 {
     public class OPtion
     {
+        public bool IsLoaded { get; private set; }
 
+        public JObject Data { get; set; } = new JObject();
+
+        public OPtion()
+        {
+            IsLoaded = false;
+        }
+
+        public void Load(string path)
+        {
+            string text = File.ReadAllText(path);
+            LoadText(text);
+        }
+
+        public void LoadText(string text)
+        {
+            Data = JObject.Parse(text);
+            IsLoaded = true;
+        }
+
+        public void Save(string path)
+        {
+            File.WriteAllText(path, Data.ToString());
+        }
+
+        public void Sync(JObject oldToken, JObject newToken)
+        {
+            foreach (var oldItem in oldToken)
+            {
+                foreach (var newItem in newToken)
+                {
+                    if (oldItem.Key == newItem.Key)
+                    {
+                        newToken[newItem.Key] = oldItem.Value;
+                    }
+                }
+            }
+        }
     }
 }
